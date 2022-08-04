d4a6bebc9af4c54ff8d97ded7a53762ed9be4dfb
dodisutarya <ikhwands@protonmail.ch>
2022-08-04 22:16:19 +0700

    ready to deploy

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -7,7 +7,7 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyCGAdd6B0x8o3VbePSlD6NgPk4xvsTV7g4",
+    apiKey: process.env.REACT_APP_FIREBASE_KEY,
     authDomain: "dos-apps-auth.firebaseapp.com",
     projectId: "dos-apps-auth",
     storageBucket: "dos-apps-auth.appspot.com",
